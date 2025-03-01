66ab9109996b38306e6265c2289645fc1e4119ef
Hiep Phan <phhiep999@gmail.com>
2025-03-01 13:32:01 +0700

    mapping database

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const { connectDB, sequelize } = require("./src/configs/database");
+const Application = require("./src/models/Application");
+const Company = require("./src/models/Company");
+const Job = require("./src/models/Job");
+const Resume = require("./src/models/Resume");
+const Review = require("./src/models/Review");
+const SaveJob = require("./src/models/SaveJob");
+const User = require("./src/models/User");
+
+const syncDatabase = async () => {
+  try {
+    await sequelize.sync({ force: true });
+    console.log("Data base sync successfully");
+  } catch (err) {
+    console.log("Data base sync failed: ", err);
+  }
+};
 
 app.get("/", (req, res) => {
   res.send("Hello World !");
@@ -9,3 +26,6 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+connectDB();
+// syncDatabase();
