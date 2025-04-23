75192ffb99495b6fdfdc22d4efde8f38ae730514
Hiep Phan <phhiep999@gmail.com>
2025-04-23 12:26:20 +0700

    edit tf-idf function

--- src/utils/tfidf.js
+++ src/utils/tfidf.js
@@ -3,21 +3,45 @@ const Tfidf = natural.TfIdf;
 
 // Hàm tính điểm TF-IDF
 
-const getTfidfScore = (text, jobIds) => {
+// const getTfidfScore = (text, jobIds) => {
+//   const tfidf = new Tfidf();
+
+//   // Giả sử bạn có các mô tả công việc của người dùng đã lưu và ứng tuyển
+//   jobIds.forEach((jobId) => {
+//     const jobDescription = getJobDescriptionById(jobId); // Hàm giả định lấy mô tả công việc theo ID
+//     tfidf.addDocument(jobDescription);
+//   });
+
+//   // Tính điểm TF-IDF cho công việc hiện tại
+//   const jobTFIDFScore = tfidf.tfidfs(text, (i, measure) => {
+//     if (i === 0) return measure; // Sử dụng mức độ TF-IDF của công việc hiện tại
+//   });
+
+//   return jobTFIDFScore[0] || 0; // Trả về điểm TF-IDF hoặc 0 nếu không có điểm
+// };
+
+/**
+ * @param {string} targetText - Văn bản cần tính điểm tương đồng (ví dụ: mô tả ứng viên, mô tả công việc mới)
+ * @param {string[]} comparedTexts - Danh sách văn bản dùng để so sánh
+ * @returns {number} - Điểm TF-IDF cao nhất
+ */
+const getTfidfScore = (targetText, comparedTexts) => {
   const tfidf = new Tfidf();
 
-  // Giả sử bạn có các mô tả công việc của người dùng đã lưu và ứng tuyển
-  jobIds.forEach((jobId) => {
-    const jobDescription = getJobDescriptionById(jobId); // Hàm giả định lấy mô tả công việc theo ID
-    tfidf.addDocument(jobDescription);
+  // Thêm tất cả văn bản vào corpus
+  comparedTexts.forEach((text) => {
+    tfidf.addDocument(text);
   });
 
-  // Tính điểm TF-IDF cho công việc hiện tại
-  const jobTFIDFScore = tfidf.tfidfs(text, (i, measure) => {
-    if (i === 0) return measure; // Sử dụng mức độ TF-IDF của công việc hiện tại
+  // So sánh văn bản cần tìm với toàn bộ document
+  let maxScore = 0;
+  tfidf.tfidfs(targetText, (i, measure) => {
+    if (measure > maxScore) {
+      maxScore = measure;
+    }
   });
 
-  return jobTFIDFScore[0] || 0; // Trả về điểm TF-IDF hoặc 0 nếu không có điểm
+  return maxScore;
 };
 
 module.exports = { getTfidfScore };
