4d6fe501cc604824596c2f39ca2a6480047ef29a
Hiep Phan <phhiep999@gmail.com>
2025-04-23 12:28:22 +0700

    create api getAllCandidate for Recruiter

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const { 
-    saveJobByUser,
-    getJobsFavorite,
-    unsaveJobByUser,
-    saveUserMajors,
-    getUserInterestedMajors,
-    getUserSkills
- } = require("../controllers/userController");
+const {
+  saveJobByUser,
+  getJobsFavorite,
+  unsaveJobByUser,
+  saveUserMajors,
+  getUserInterestedMajors,
+  getUserSkills,
+  getAllCandidates,
+} = require("../controllers/userController");
 const { verifyToken } = require("../middlewares/authMiddlewares");
 
+// Lấy tất cả ứng viên
+
+router.get("/getAllCandidates/:recruiterId", verifyToken, getAllCandidates);
 router.post("/savejob/:jobId", verifyToken, saveJobByUser);
-router.get("/favorites", verifyToken , getJobsFavorite);
+router.get("/favorites", verifyToken, getJobsFavorite);
 router.delete("/unsavejob/:jobId", verifyToken, unsaveJobByUser);
 router.post("/:userId/majors", verifyToken, saveUserMajors);
 router.get("/:userId/majors", verifyToken, getUserInterestedMajors);
