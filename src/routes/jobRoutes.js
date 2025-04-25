507fd469bd8d38aa2100c0198605e8e6940d071d
Hiep Phan <phhiep999@gmail.com>
2025-04-25 14:36:13 +0700

    add api: update Job

--- src/routes/jobRoutes.js
+++ src/routes/jobRoutes.js
@@ -1,23 +1,25 @@
 const express = require("express");
 const {
   createJob,
+  updateJob,
   getAllJobsByRecruiter,
   // getJobByJobId,
   getAllJobsByCandidate,
   getJobDetailByCandidate,
   getJobsByAdmin,
-  getJobDetailByAdmin
+  getJobDetailByAdmin,
 } = require("../controllers/jobController");
 const { verifyToken } = require("../middlewares/authMiddlewares");
-const { optionalVerifyToken } = require("../middlewares/optionalMiddlewares")
+const { optionalVerifyToken } = require("../middlewares/optionalMiddlewares");
 
 const router = express.Router();
 
 router.post("/", createJob); // Create a job
+router.put("/:id", verifyToken, updateJob); // Update a job
 router.get("/recruiter/:recruiter_id", getAllJobsByRecruiter); // Get all jobs by recruiter id
 // router.get("/job/:id", getJobByJobId);
-router.get("/", optionalVerifyToken ,getAllJobsByCandidate);
+router.get("/", optionalVerifyToken, getAllJobsByCandidate);
 router.get("/detail/:id", getJobDetailByCandidate);
-router.get("/admin/",getJobsByAdmin)
-router.get("/admin/:id",getJobDetailByAdmin)
+router.get("/admin/", getJobsByAdmin);
+router.get("/admin/:id", getJobDetailByAdmin);
 module.exports = router;
