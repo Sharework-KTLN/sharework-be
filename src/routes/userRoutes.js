425da57ff55241bc2039fa0f91582bd505e333d7
Hiep Phan <phhiep999@gmail.com>
2025-05-14 16:29:40 +0700

    add: api gợi ý ứng viên phù hợp

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,24 +1,30 @@
 const express = require("express");
 const router = express.Router();
-const { 
-    saveJobByUser,
-    getJobsFavorite,
-    unsaveJobByUser,
-    saveUserMajors,
-    getUserInterestedMajors,
-    getUserSkills,
-    getAllUsers,
-    getUserDetail,
-    getDashboardStats,
-    getMonthlyStats,
-    getAllCandidates,
-    getJobsApplied
- } = require("../controllers/userController");
+const {
+  saveJobByUser,
+  getJobsFavorite,
+  unsaveJobByUser,
+  saveUserMajors,
+  getUserInterestedMajors,
+  getUserSkills,
+  getAllUsers,
+  getUserDetail,
+  getDashboardStats,
+  getMonthlyStats,
+  getAllCandidates,
+  getJobsApplied,
+  getAllCandidatesMatchWithJob,
+} = require("../controllers/userController");
 const { verifyToken } = require("../middlewares/authMiddlewares");
 
 // Lấy tất cả ứng viên
 
 router.get("/getAllCandidates/:recruiterId", verifyToken, getAllCandidates);
+router.get(
+  "/getAllCandidatesMatchWithJob/:jobId",
+  verifyToken,
+  getAllCandidatesMatchWithJob
+);
 router.post("/savejob/:jobId", verifyToken, saveJobByUser);
 router.get("/favorites", verifyToken, getJobsFavorite);
 router.get("/applies", verifyToken, getJobsApplied);
@@ -27,7 +33,7 @@ router.post("/:userId/majors", verifyToken, saveUserMajors);
 router.get("/:userId/majors", verifyToken, getUserInterestedMajors);
 router.get("/:userId/skills", verifyToken, getUserSkills);
 router.get("/alluser", getAllUsers);
-router.get('/detail/:id', getUserDetail);
-router.get('/admin/dashboard', getDashboardStats);
-router.get('/admin/monthlystats', getMonthlyStats);
+router.get("/detail/:id", getUserDetail);
+router.get("/admin/dashboard", getDashboardStats);
+router.get("/admin/monthlystats", getMonthlyStats);
 module.exports = router;
