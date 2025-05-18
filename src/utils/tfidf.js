b82a4d40f048b7161c290be0a5a0b7a4ac1b0b7d
Nhut Hao <c520a1@gmail.com>
2025-05-18 15:35:24 +0700

    fix TF-IDF, recommendApplied

--- src/utils/tfidf.js
+++ src/utils/tfidf.js
@@ -1,30 +1,107 @@
 const natural = require("natural");
 const Job = require("../models/Job");
 const Tfidf = natural.TfIdf;
+const tokenizer = new natural.WordTokenizer();
+
+const stopwords = new Set([
+  "và", "là", "của", "có", "cho", "để", "trong", "các", "một", "những", "với",
+  "đã", "ra", "đó", "này", "tôi", "bạn", "anh", "chị", "em", "chúng", "họ",
+  "cũng", "như", "này", "làm", "tôi", "được", "đến", "còn", "nếu", "thì", "ở",
+]);
+
+function preprocess(text) {
+  if (!text) return "";
+
+  // Chuyển chữ thường
+  let lowerText = text.toLowerCase();
+
+  // Loại bỏ dấu câu và ký tự đặc biệt, thay bằng khoảng trắng
+  lowerText = lowerText.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()@\+\?><\[\]\+"]/g, " ");
+
+  // Tách từ
+  const tokens = tokenizer.tokenize(lowerText);
+
+  // Lọc stopwords
+  const filteredTokens = tokens.filter(token => !stopwords.has(token));
+
+  // Nối lại thành chuỗi
+  return filteredTokens.join(" ");
+}
+
+const extractMainJobTitle = (title) => {
+  if (!title) return "";
+  const parts = title.split("/");
+  return parts[parts.length - 1].trim();
+};
 
 const getJobDescriptionById = async (jobId) => {
   const job = await Job.findByPk(jobId, {
     attributes: ["title", "description"],
   });
   if (!job) return "";
-  return `${job.title} ${job.description}`;
+
+  const mainTitle = extractMainJobTitle(job.title);
+  const fullText = `${mainTitle} ${job.description}`;
+  return preprocess(fullText);
 };
 
 // Hàm tính điểm TF-IDF
+// const getTfidfScore = async (text, jobIds) => {
+//   const tfidf = new Tfidf();
+
+//   for (const jobId of jobIds) {
+//     const jobDescription = await getJobDescriptionById(jobId);
+//     tfidf.addDocument(jobDescription);
+//   }
+
+//   let score = 0;
+//   tfidf.tfidfs(text, function (i, measure) {
+//     if (i === 0) score = measure;
+//   });
+
+//   return score;
+// };
+// const getTfidfScore = async (text, jobIds) => {
+//   const tfidf = new Tfidf();
+
+//   // Thêm từng mô tả công việc vào tfidf corpus
+//   for (const jobId of jobIds) {
+//     const jobDescription = await getJobDescriptionById(jobId);
+//     tfidf.addDocument(jobDescription);
+//   }
+
+//   let maxScore = 0;
+
+//   // Tính điểm TF-IDF giữa text với từng document
+//   tfidf.tfidfs(text, function (i, measure) {
+//     if (measure > maxScore) {
+//       maxScore = measure;
+//     }
+//   });
+
+//   return maxScore;
+// };
 const getTfidfScore = async (text, jobIds) => {
   const tfidf = new Tfidf();
 
-  for (const jobId of jobIds) {
-    const jobDescription = await getJobDescriptionById(jobId);
-    tfidf.addDocument(jobDescription);
-  }
+  // Lấy mô tả của các job đã lưu
+  let jobDescriptions = await Promise.all(
+    jobIds.map((id) => getJobDescriptionById(id))
+  );
 
-  let score = 0;
-  tfidf.tfidfs(text, function (i, measure) {
-    if (i === 0) score = measure;
+  // Loại bỏ mô tả trùng nhau (giúp tránh nhân đôi điểm)
+  jobDescriptions = [...new Set(jobDescriptions)];
+
+  jobDescriptions.forEach((desc) => tfidf.addDocument(desc));
+
+  const cleanText = preprocess(text);
+
+  let maxScore = 0;
+  tfidf.tfidfs(cleanText, (i, measure) => {
+    if (measure > maxScore) maxScore = measure;
   });
 
-  return score;
+  return maxScore;
 };
 
 /**
@@ -51,4 +128,4 @@ const getTfidfScoreRecruiter = (targetText, comparedTexts) => {
   return maxScore;
 };
 
-module.exports = { getTfidfScore, getTfidfScoreRecruiter };
+module.exports = { getTfidfScore, getTfidfScoreRecruiter, getJobDescriptionById, extractMainJobTitle, preprocess};
